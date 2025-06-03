cb8339b8312fbf23c84ac058dd0bd6e12029f3cf
Volodya Sr <pere74916@gmail.com>
2025-06-03 09:47:09 +0200

    Shunting yard

---
+++ Lexer.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Lexer {
+    private char[] tokens;
+    private int index = -1;
+
+    public Lexer(String srt) {
+        this.tokens = srt.toCharArray();
+    }
+
+    public char[] tokenize() {
+        while (tokens.hasNext) {
+
+        }
+        return null;
+    }
+
+    private boolean hasNext() {
+        tokens();
+    }
+}
---
+++ Saki.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Saki {
+    public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    Stack<Character> operands = new Stack<>();
+	    List<Character> output = new ArrayList<>();
+	    
+        String userInput = scanner.nextLine();
+        
+    }
+}
